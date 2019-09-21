1602b5fd87da74e849737dd32f2189f5fbad88d6
ifsl2 <ifsl2@cin.ufpe.br>
2019-09-21 00:11:36 +0000

    Inicializando Login

--- FOURR/config/routes.rb
+++ FOURR/config/routes.rb
@@ -1,3 +1,13 @@
+=begin
 Rails.application.routes.draw do
+  get 'login/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+=end
+Rails.application.routes.draw do
+    resources :users
+    get    'sign_in'   => 'login#new'
+    post   'sign_in'   => 'login#create'
+    delete 'sign_out'  => 'login#destroy'
 end
