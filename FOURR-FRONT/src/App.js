17e5382d73c8db6c3d1b4719462b2bae0b61b0dd
jtomaz99 <jtomaz1999@gmail.com>
2019-09-24 20:49:43 -0300

    #21 Criação da página de cadastro e criação das rotas

--- FOURR-FRONT/src/App.js
+++ FOURR-FRONT/src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCheckCircle, faTrashAlt)
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
