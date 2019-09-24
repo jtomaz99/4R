9b051030e02b86fae6385d239792df2b437de26e
jtomaz99 <jtomaz1999@gmail.com>
2019-09-24 08:14:47 -0300

    #26 inicialização do Rspec

---
+++ FOURR/spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe 'validate name' do
+    it 'create api' do
+      tutorial = WelcomeController.new()
+    end
+  end
+end
