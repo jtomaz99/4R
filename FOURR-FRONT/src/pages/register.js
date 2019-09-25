af9b0234a748ce8c8009773b5993f0aab065ef9b
jtomaz99 <jtomaz1999@gmail.com>
2019-09-24 22:54:32 -0300

    #21 progresso na construção da React View.

--- FOURR-FRONT/src/pages/register.js
+++ FOURR-FRONT/src/pages/register.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import './register.css';
 
 import logo from '../assets/logo.svg';
+import retangulo1 from '../assets/Rectangle2.1.svg'
 
-export default function Login({ history }) {
-    const [username, setUsername] = useState('');
+export default function Register({ history }) {
+    const [username, lastname, email, password,passwordconfirmation, setUsername, setLastname, setEmail, setPassword] = useState('');
 
 //    async function handleSubmit(e) {
 //        e.preventDefault();
@@ -22,11 +23,31 @@ export default function Login({ history }) {
     return(
         <div className="login-container">
             <form onSubmit=''>
-                <img src={logo} alt="Tindev"/>
-                <input placeholder="Digite seu usuario do Github"
-                    placeholder="Digite seu usuário no Github"
+                <img src={logo} alt="FOURR"/>
+                <h5><b>Crie sua conta</b></h5>
+                <input placeholder="Nome"
                     value={username}
                     onChange={e => setUsername(e.target.value)}
+                />
+
+                <input placeholder="Sobrenome"
+                    value={lastname}
+                    onChange={e => setLastname(e.target.value)}
+                />
+
+                <input placeholder="E-Mail"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                />
+
+                <input placeholder="Senha"
+                value={password}
+                onChage={e => setPassword(e.target.value)}
+                />
+
+                <input placeholder="Confirme sua senha"
+                value={passwordconfirmation}
+                onChange={e => setPassword(e.target.value)}
                 />
                 <button type="submit">Entrar</button>
             </form>
