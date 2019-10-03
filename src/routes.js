021faa21366dd498076fa4726ca66bb34bc1baf7
jtomaz99 <jtomaz1999@gmail.com>
2019-10-03 15:43:44 -0300

    correção do css doe cadastro e login

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Login from './login';
+import Login from './pages/login';
+import Register from './pages/register';
 
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Route path='/' exact component={Login} />
+            <Route path='/register' exact component={Register} />
         </BrowserRouter>
     );
 }
