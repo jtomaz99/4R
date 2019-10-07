b0efb65684faaef232d860f2ded1bbac6eb09759
Vitor Lima <vals@cin.ufpe.br>
2019-10-07 08:13:55 -0300

    preparando rotas para lidar com redirect

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/login';
 import Register from './pages/register';
 
 
-export default function Routes() {
-    return(
-        <BrowserRouter>
-            <Route path='/' exact component={Login} />
-            <Route path='/register' exact component={Register} />
-        </BrowserRouter>
-    );
+export default class Routes extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			logged_in: false,
+			user: {}
+		}
+	}
+    render() {
+    	return (
+	        <BrowserRouter>
+	            <Route 
+	            	path='/' 
+	            	exact 
+	            	render={props => (
+	            		<Login {... props} logged_in={this.state.logged_in} />
+	            		)}
+
+	            	/>
+	            <Route 
+	            	path='/register' 
+	            	exact 
+	            	component={props => (
+	            		<Register {... props} logged_in={this.state.logged_in} />
+	            		)}
+	            	/>
+	        </BrowserRouter>
+    	);
+	}
 }
