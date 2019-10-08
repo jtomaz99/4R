56a80f9a1b03577c8caa8661f87f238a4e09d2be
vitorlima-vl <vals@cin.ufpe.br>
2019-10-07 22:12:45 -0300

    fixing loginStatus funcion

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@ export default class Routes extends Component {
 		
 		this.handeLogin = this.handleLogin.bind(this);
 		this.handeLogout = this.handleLogout.bind(this);
-		this.loginStatus = this.loginStatus();
+		this.loginStatus = this.loginStatus.bind(this);
 	}
 	
 	loginStatus(){
