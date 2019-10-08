bfaa9d1bcdbfffb12d1ed4786f372fc6be9416f6
vitorlima-vl <vals@cin.ufpe.br>
2019-10-07 22:44:11 -0300

    console.log teste

--- src/routes.js
+++ src/routes.js
@@ -25,11 +25,7 @@ export default class Routes extends Component {
 		axios
 			.get("https://fourr-api.herokuapp.com/logged_in", { withCredentials: true })
 			.then(response => {
-				if (response.data.logged_in && this.state.logged_in === "nao_logado")
-					console.log(response)
-				else if (!response.data.logged_in && this.state.logged_in === "logado" )
-					console.log(response)
-				}
+				console.log(response)
 			)
 			.catch(error =>{
 				console.log("check login error",error);
