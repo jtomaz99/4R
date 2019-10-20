22258f4d479d7c041dec526bfd14e52175b74d37
Vitor Lima <vals@cin.ufpe.br>
2019-10-20 19:00:36 -0300

    fix image upload

--- src/pages/cadastro_prod.js
+++ src/pages/cadastro_prod.js
@@ -57,7 +57,7 @@ export default class Cadastro_prod extends Component {
                 if (response.data.status === true){
                     const formData = new FormData();
                     formData.append('image',this.state.imagem,this.state.imagem.name)
-                    axios.post("https://fourr-api.herokuapp.com/new_product_img/",{
+                    axios.post("https://fourr-api.herokuapp.com/new_img/",{
                         imagem:{formData:formData,
                                 produto:response.data.produto.id}},
                         {withCredentials: true}
