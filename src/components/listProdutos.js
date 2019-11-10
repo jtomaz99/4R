7094c538d490440a883290456cb25b887e6ff6c5
Vitor Lima <vals@cin.ufpe.br>
2019-11-10 11:26:55 -0300

    see type

--- src/components/listProdutos.js
+++ src/components/listProdutos.js
@@ -3,6 +3,7 @@ import React from "react"
 function ListProdutos(produtos) {
 	return (
 	<div>
+	console.log(typeof(produtos))
     {produtos.map((produto, index) => {
     	return <h1>{produto.nome_prod}</h1>
     })}
--- src/pages/produtos_busca.js
+++ src/pages/produtos_busca.js
@@ -26,7 +26,6 @@ export default class Home extends Component {
 	}	
 	componentDidMount(){
 		console.log(this.props.produtos)
-		typeof(this.props.produtos)
 	}
 
 	handlePesquisarClick(){
@@ -59,7 +58,7 @@ export default class Home extends Component {
 					</div>
 					<div className="col">
 						<h1>PRODUTINHOS</h1>
-						//<ListProdutos produtos={this.props.produtos}/>
+						<ListProdutos produtos={this.props.produtos}/>
 					</div>
 				</div>
             </div>
