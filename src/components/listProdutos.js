083b40fc819c350852a693563236f379c9597091
Vitor Lima <vals@cin.ufpe.br>
2019-11-10 11:35:35 -0300

    see type

--- src/components/listProdutos.js
+++ src/components/listProdutos.js
@@ -2,8 +2,9 @@ import React from "react"
 
 function ListProdutos(produtos) {
 	return (
-	<div>
 	console.log(typeof(produtos))
+	<div>
+	
     //{produtos.map((produto, index) => {
     //	return <h1>{produto.nome_prod}</h1>
     //})}
