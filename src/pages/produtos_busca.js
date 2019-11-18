c913bbbd8d131ba57b6b9dddda3518e971c52905
vgss <vgss@cin.ufpe.br>
2019-11-18 19:14:39 -0300

    bugfixes produtos_busca

--- src/pages/produtos_busca.js
+++ src/pages/produtos_busca.js
@@ -74,19 +74,27 @@ export default class Home extends Component {
 						<button type="button" className="btn btn-success item" onClick={() => this.handleLogoutClick()} >Sair</button>
 					</div>
 				</div>
-				<div>
+				<div className="container-items">
 					<h1>Resultado da busca</h1>
+					<div className="row">
 					{this.props.produtos.map((produto, index) => {
-						return <div className="card results" >
-								<div className="card-body">
-						  			<h5 className="card-title">{produto.nome_prod}</h5>
-						  			<p className="card-text">Descrição: {produto.descricao}</p>
-									<p className="card-text">Categoria: {produto.categoria}</p>
-									<button type="button" class="btn btn-primary" onClick={() => this.solicitarItem(produto.dono_produto)} >Solicitar Produto</button>
-								</div>
-					  		   </div>
+						return  <div className="col-lg-4 col-md-6 mb-4">
+									<div className="card h-100">
+										<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+										<div className="card-body">
+						  					<h4 className="card-title">{produto.nome_prod}</h4>
+											<h5>{produto.categoria}</h5>
+						  					<p className="card-text">Descrição: {produto.descricao}</p>
+										</div>
+										<div class="card-footer">
+											<button type="button" class="btn btn-primary" onClick={() => this.solicitarItem(produto.dono_produto)} >Solicitar Produto</button>
+              							</div>
+									</div>
+					  		   	</div>
 				    })}
+					</div>
 				</div>
+
 				{this.state.showPopup ? <Popup text= {this.state.donoP}
                             closePopup={this.togglePopup.bind(this)}/>: null}
             </div>
