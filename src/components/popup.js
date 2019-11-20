12ac64b1fcdbce043892684b3f9894b5467d7c6f
vgss <vgss@cin.ufpe.br>
2019-11-20 19:22:44 -0300

    update popup

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,30 +1,17 @@
 import React from 'react';  
 
 class Popup extends React.Component {  
-  render() {  
-	return (  
-		<div class="modal" tabindex="-1" role="dialog">
-			  <div class="modal-dialog" role="document">
-				<div class="modal-content">
-					  <div class="modal-header">
-						   <h5 class="modal-title">Alerta para detalhes:</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-							  <span aria-hidden="true">&times;</span>
-						</button>
-					  </div>
-					  <div class="modal-body">
-						<p>{this.props.text}</p>
-					 </div>
-					  <div class="modal-footer">
-						<button type="button" class="btn btn-primary" data-dismiss="modal" onClick={this.props.closePopup} >Fechar </button>
-					 </div>
-				</div>
-			  </div>
-		
-		 </div>
-		
+	render() {  
+		return (  
+			<div className='popup'>  
+			<div className='popup\_inner'>  
+			<h1>{this.props.text}</h1>  
+			<button onClick={this.props.closePopup}>Fechar</button>  
+			</div>  
+			</div>  
 		);  
 		}  
 		}  
+
 
 export default Popup;
